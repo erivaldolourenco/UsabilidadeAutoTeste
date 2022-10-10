adcdd9f9b92a292b4ca8b0ca922a44a931868c88
Erivaldo Lourenço <erivaldo.mariano@pdcase.com.br>
2022-10-10 00:01:04 -0300

    Initial commit

---
+++ src/main/java/br/com/doity/usabilidadeautoteste/UsabilidadeAutoTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.doity.usabilidadeautoteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsabilidadeAutoTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsabilidadeAutoTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/doity/usabilidadeautoteste/UsabilidadeAutoTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doity.usabilidadeautoteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsabilidadeAutoTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
